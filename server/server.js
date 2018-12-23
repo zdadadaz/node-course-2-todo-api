66ef56e3e030f095cfe7ea2ae75103c6a2d95164
zdadadaz <zdadadaz@yahoo.com.tw>
2018-12-23 12:25:51 +1000

    Add GET todos

--- server/server.js
+++ server/server.js
@@ -23,6 +23,16 @@ app.post('/todos',(req,res)=>{
     //console.log(req.body);
 });
 
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send(todos)
+    },(e)=>{
+        res.status(400).send(e);
+    });
+});
+
+
+
 app.listen('3000',()=>{
     console.log('Started on port 3000');
 })
