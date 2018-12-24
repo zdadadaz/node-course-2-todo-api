4d5d05365a158002882f6d5f27410f01b768d97c
zdadadaz <zdadadaz@yahoo.com.tw>
2018-12-24 16:41:14 +1000

    setup for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://zdadadaz:chiou5917@ds019482.mlab.com:19482/to-do-jcc');
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports={
     mongoose
