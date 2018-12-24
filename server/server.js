d8dd501675c530a204472e4165fbf44c6295a45c
zdadadaz <zdadadaz@yahoo.com.tw>
2018-12-24 17:53:43 +1000

    add patch

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -65,6 +66,27 @@ app.delete('/todos/:id',(req,res)=>{
     });
 });
 
+app.patch('/todos/:id',(req,res)=>{
+   var id = req.params.id;
+   var body = _.pick(req.body,['text', 'completed']);
+   if(!ObjectID.isValid(id)){
+      return res.status(404).send('ID no found');
+   }
+   if(_.isBoolean(body.completed)&& body.completed){
+       body.completedAt = new Date().getTime();
+   } else{
+       body.completed = false;
+       body.completedAt = null;
+   }
+   Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
+    if(!todo){
+        return res.status(404).send();
+    }
+    res.send({todo});
+   }).catch((e)=>{
+       res.status(400).send();
+   })
+});
 
 app.listen(port,()=>{
     console.log(`start up with ${port}`);
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const expect = require('expect');
 const request = require('supertest');
 
