40e4da47d021c726321b45314addc264db8451b1
zdadadaz <zdadadaz@yahoo.com.tw>
2018-12-27 15:25:32 +1000

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -88,6 +88,25 @@ UserSchema.statics.findByToken = function (token) {
 
     });
 };
+UserSchema.statics.findByCredentials = function(email,password){
+    var User = this;
+
+    return User.findOne({email}).then((user)=>{
+        if(!user){
+            return Promise.reject();
+        }
+        return new Promise((resovle,reject)=>{
+            bcrypt.compare(password,user.password,(err,res)=>{
+                if(res){
+                    resovle(user);
+                }else{
+                    reject();
+                }
+            });
+        });
+    });
+};
+
 // middleware to hash password
 UserSchema.pre('save',function(next){
     var user = this;
